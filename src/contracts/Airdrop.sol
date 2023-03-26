601eabd3900477f717fd3452fc74f2ab558c5c8f
Sherkhan Azimov <sherkhanazim@gmail.com>
2023-03-26 16:21:34 +0300

    fix(): airdrop format

--- src/contracts/Airdrop.sol
+++ src/contracts/Airdrop.sol
@@ -3,18 +3,18 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract AirdropToken{
+contract AirdropToken {
     function airdropWithTransfer(
-        address _tokenAddress, 
-        address[] calldata _addressArray, 
-        uint256[] calldata _amountArray) public {
-            for (uint8 i = 0; i<_addressArray.length; i++) 
-            {
-                 IERC20(_tokenAddress).transfer(_addressArray[i], _amountArray[i]);
-            }
+        address _tokenAddress,
+        address[] calldata _addressArray,
+        uint256[] calldata _amountArray
+    ) public {
+        for (uint8 i = 0; i < _addressArray.length; i++) {
+            IERC20(_tokenAddress).transfer(_addressArray[i], _amountArray[i]);
+        }
     }
 
-    function checkBalance(address _tokenAddress) view public returns (uint256) {
+    function checkBalance(address _tokenAddress) public view returns (uint256) {
         return IERC20(_tokenAddress).balanceOf(address(this));
     }
 }
